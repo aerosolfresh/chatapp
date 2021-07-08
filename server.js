b7181e05763baf0c5a9ec4f97b2483c379e9bab6
Toshi <tosmow@gmail.com>
2021-07-08 11:45:09 -0600

    server update

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const {Chat} = require("./model");
 const server = express();
+
+server.use(cors);
+server.use(express.json());
+server.use(express.static('static'));
