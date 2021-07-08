da22f6c629e7e2e8b987a20d55307436d31ff87f
Aris <nasiraselano@gmail.com>
2021-07-08 11:52:25 -0600

    createMsg() function created

--- static/app.js
+++ static/app.js
@@ -5,11 +5,30 @@ var app = new Vue ({
         body:"",
         new_author:"",
         new_body:"",
+        // test values
+        msgs:[
+            {
+                author:"User",
+                body:"this is a msg"
+            }
+        ]
     },
     methods:{
+        createMsg:function(){    
+        // var for a new thread
+        var new_msg={
+            author:this.new_author,
+            body:this.new_body,
+        }
+        //push the new thread to threads list
+        this.threads.unshift(new_msg)
 
+        //clear the inputs
+        this.new_author="";
+        this.new_body="";
+        }
     },
     computed:{
-        
+
     }
 });
